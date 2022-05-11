3c6522e5b49b203660e6940cbc6af8e544008757
Priscilla Alves <perealvees@gmail.com>
2022-05-11 18:23:29 -0300

    ajuste de css

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ let sliderElement = document.querySelector("#slider");
 let buttomElement = document.querySelector("#button");
 
 let sizePassword = document.querySelector("#valor");
-let password = document.querySelector("password");
+let password = document.querySelector("#password");
 
-let containerPassword = document.querySelector("#containerPassword");
+let containerPassword = document.querySelector("#container-password");
 
 let charset = "abdefg12345";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.this.value;
 
-sliderElement.oninput = function(){
+slider.oninput = function(){
     sizePassword.innerHTML= this.Value;
 }
 
 function generatePassword(){
     
     let pass = "";
-
     for(let i = 0, n = charset.length; i <sliderElement.value; ++i){
         pass += charset.charAt(Math.floor(Math.random() * n));
     }
 
     containerPassword.classList.remove("hide");
     password.innerHTML = pass;
+    novaSenha = pass;
 }
 
 function copyPassword(){
